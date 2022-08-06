b9f16c13e9e45cd97f5977538db879e58d46cee0
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-08-06 12:11:04 +0500

    WIP

--- lib/latest_news_page.dart
+++ lib/latest_news_page.dart
@@ -1,8 +1,13 @@
 import "package:flutter/material.dart";
 
-class LatestNewsPage extends StatelessWidget {
+class LatestNewsPage extends StatefulWidget {
   const LatestNewsPage({Key? key}) : super(key: key);
 
+  @override
+  State<LatestNewsPage> createState() => _LatestNewsPageState();
+}
+
+class _LatestNewsPageState extends State<LatestNewsPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
