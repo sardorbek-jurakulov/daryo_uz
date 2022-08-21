704d22f8f67002c321639e98ebae3a81cd27463d
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-08-21 17:39:29 +0500

    WIP

--- lib/news_details_page.dart
+++ lib/news_details_page.dart
@@ -28,108 +28,151 @@ class _NewsDetailsPageState extends State<NewsDetailsPage> {
           decoration: const BoxDecoration(
             color: Color(0xfff3f3f3),
           ),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              decoration: const BoxDecoration(
-                color: Color(0xffffffff),
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: ListView(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  decoration: const BoxDecoration(
+                    color: Color(0xffffffff),
+                  ),
+                  child: Column(
                     children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "${args["createdTime"]} | ${args["createdDate"]}" ??
+                                "yangilik vaqti",
+                            style: const TextStyle(
+                              color: Color(0xffababab),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.remove_red_eye_outlined,
+                                color: Color(0xff337b94),
+                              ),
+                              const SizedBox(
+                                width: 2,
+                              ),
+                              Text(
+                                args["numberOfViews"] ?? "ko'rishlar soni",
+                                style: const TextStyle(
+                                  color: Color(0xff337b94),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
                       Text(
-                        "${args["createdTime"]} | ${args["createdDate"]}" ??
-                            "yangilik vaqti",
+                        args["title"] ?? "Sarlavha",
                         style: const TextStyle(
-                          color: Color(0xffababab),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        args["tldr"] ?? "Sarlavha",
+                        textAlign: TextAlign.justify,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Image.asset(args["imagePath"] ?? "Rasm"),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        'Foto: ${args["imageSource"] ?? "rasm manbasi"}',
+                        textAlign: TextAlign.start,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        args["description"] ?? "Batafsil",
+                        textAlign: TextAlign.justify,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 5, vertical: 10),
+                        decoration: const BoxDecoration(
+                          color: Color(0xff2ca5df),
+                        ),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              "assets/images/telegram.png",
+                              width: 35,
+                              height: 35,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            const Expanded(
+                              child: Text(
+                                "Telegramingizda hali ham \"Daryo\" ning rasmiy kanali yo'qmi? Join!",
+                                style: TextStyle(
+                                  color: Color(0xffffffff),
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Icon(
-                            Icons.remove_red_eye_outlined,
-                            color: Color(0xff337b94),
-                          ),
-                          const SizedBox(
-                            width: 2,
-                          ),
-                          Text(
-                            args["numberOfViews"] ?? "ko'rishlar soni",
-                            style: const TextStyle(
-                              color: Color(0xff337b94),
+                          const Text("Reklama"),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.only(
+                                left: 25,
+                              ),
+                              child: const Divider(
+                                height: 50,
+                                thickness: 1,
+                                color: Color(0xffc8ccd0),
+                              ),
                             ),
                           ),
                         ],
                       ),
+                      Container(
+                        child: Image.asset(
+                          "assets/images/anorbank_reklama.jpeg",
+                          fit: BoxFit.fill,
+                        ),
+                      ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    args["title"] ?? "Sarlavha",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    args["tldr"] ?? "Sarlavha",
-                    textAlign: TextAlign.justify,
-                  ),
-                  Image.asset(args["imagePath"] ?? "Rasm"),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    'Foto: ${args["imageSource"] ?? "rasm manbasi"}',
-                    textAlign: TextAlign.start,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    args["description"] ?? "Batafsil",
-                    textAlign: TextAlign.justify,
-                  ),
-                  Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-                    decoration: const BoxDecoration(
-                      color: Color(0xff2ca5df),
-                    ),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          "assets/images/telegram.png",
-                          width: 35,
-                          height: 35,
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        const Expanded(
-                          child: Text(
-                            "Telegramingizda hali ham \"Daryo\" ning rasmiy kanali yo'qmi? Join!",
-                            style: TextStyle(
-                              color: Color(0xffffffff),
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Boshqa yangiliklar"),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
