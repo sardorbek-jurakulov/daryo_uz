ee755c85a72bedadabce99490f4526260ffefdf9
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-08-21 14:45:00 +0500

    WIP

--- lib/news_details_page.dart
+++ lib/news_details_page.dart
@@ -17,34 +17,47 @@ class _NewsDetailsPageState extends State<NewsDetailsPage> {
         appBar: AppBar(
           title: Text(
             args["category"] ?? "Batafsil",
-            style: TextStyle(
+            style: const TextStyle(
               fontWeight: FontWeight.w600,
               fontSize: 19,
             ),
           ),
           centerTitle: true,
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              Row(
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xfff3f3f3),
+          ),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: const BoxDecoration(
+                color: Color(0xffffffff),
+              ),
+              child: Column(
                 children: [
-                  Text("${args["createdTime"]} | ${args["createdDate"]}" ??
-                      "yangilik vaqti"),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Icon(Icons.remove_red_eye_outlined),
-                      Text(args["numberOfViews"] ?? "ko'rishlar soni"),
+                      Text("${args["createdTime"]} | ${args["createdDate"]}" ??
+                          "yangilik vaqti"),
+                      Row(
+                        children: [
+                          const Icon(Icons.remove_red_eye_outlined),
+                          Text(args["numberOfViews"] ?? "ko'rishlar soni"),
+                        ],
+                      ),
                     ],
                   ),
+                  Text(args["title"] ?? "Sarlavha"),
+                  Text(args["tldr"] ?? "Sarlavha"),
+                  Image.asset(args["imagePath"] ?? "Rasm"),
+                  Text(args["description"] ?? "Batafsil")
                 ],
               ),
-              Text(args["title"] ?? "Sarlavha"),
-              Text(args["tldr"] ?? "Sarlavha"),
-              Image.asset(args["imagePath"] ?? "Rasm"),
-              Text(args["description"] ?? "Batafsil")
-            ],
+            ),
           ),
         ),
       ),
